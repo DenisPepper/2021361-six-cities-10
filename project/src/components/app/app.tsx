d84ab7adaccad8a2dfdb89f49d78175f841628b1
Перцев Денис <thech@yandex.ru>
2022-07-11 22:10:42 +0300

    add props to MainPage, App

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,6 +1,10 @@
 import MainPage from '../../pages/main-page/main-page';
 
+type AppProps = {
+  placesCount: number;
+}
 
-export default function App(): JSX.Element {
-  return <MainPage />;
+export default function App(props: AppProps): JSX.Element {
+  const {placesCount} = props;
+  return <MainPage placesCount = {placesCount} />;
 }
