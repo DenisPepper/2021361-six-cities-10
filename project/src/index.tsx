aa5076dd01db8ad9d3246ae543de4039aa0014bf
Перцев Денис <thech@yandex.ru>
2022-07-17 18:42:19 +0300

    delete React.StrictMode

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
 const ROOMS_COUNT = 20;
-const ROOMS = Array.from({length: ROOMS_COUNT}, (e, i) => ++i);
+const ROOMS = Array.from({ length: ROOMS_COUNT }, (e, i) => ++i);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
 
-root.render(
-  <React.StrictMode>
-    <App placesCount = {ROOMS_COUNT} rooms = {ROOMS}/>
-  </React.StrictMode>,
-);
+root.render(<App placesCount={ROOMS_COUNT} rooms={ROOMS} />);
