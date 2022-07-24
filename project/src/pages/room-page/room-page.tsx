c0a9a9c64af0bafb0b968610bd71be226da05d26
Перцев Денис <thech@yandex.ru>
2022-07-24 21:05:40 +0300

    add CommentForm FC

--- project/src/pages/room-page/room-page.tsx
+++ project/src/pages/room-page/room-page.tsx
@@ -3,6 +3,7 @@ import { OfferType } from '../../types/offer-type';
 import Page404 from '../404-page/404-page';
 import RoomImage from '../../components/room-image/room-image';
 import { converToPercent } from '../../util';
+import CommentForm from '../../components/comment-form/comment-form';
 
 const DECIMAL = 10;
 
@@ -65,13 +66,17 @@ export default function RoomPage(props: RoomPageProps): JSX.Element {
               ))}
             </div>
           </div>
+
           <div className='property__container container'>
             <div className='property__wrapper'>
-              {room.isPremium ? (<div className='property__mark'><span>Premium</span></div>) : null}
+              {room.isPremium ? (
+                <div className='property__mark'>
+                  <span>Premium</span>
+                </div>
+              ) : null}
+
               <div className='property__name-wrapper'>
-                <h1 className='property__name'>
-                  {room.title}
-                </h1>
+                <h1 className='property__name'>{room.title}</h1>
                 <button
                   className='property__bookmark-button button'
                   type='button'
@@ -86,6 +91,7 @@ export default function RoomPage(props: RoomPageProps): JSX.Element {
                   <span className='visually-hidden'>To bookmarks</span>
                 </button>
               </div>
+
               <div className='property__rating rating'>
                 <div className='property__stars rating__stars'>
                   <span style={{ width: `${converToPercent(room.rating)}%` }} />
@@ -95,6 +101,7 @@ export default function RoomPage(props: RoomPageProps): JSX.Element {
                   {room.rating}
                 </span>
               </div>
+
               <ul className='property__features'>
                 <li className='property__feature property__feature--entire'>
                   {room.type}
@@ -106,20 +113,31 @@ export default function RoomPage(props: RoomPageProps): JSX.Element {
                   {`Max ${room.maxAdults} adults`}
                 </li>
               </ul>
+
               <div className='property__price'>
                 <b className='property__price-value'>€{room.price}</b>
                 <span className='property__price-text'>&nbsp;night</span>
               </div>
+
               <div className='property__inside'>
                 <h2 className='property__inside-title'>What is inside</h2>
                 <ul className='property__inside-list'>
-                  {room.goods.map((element, index) => (<li key = {++index} className='property__inside-item'>{element}</li>))}
+                  {room.goods.map((element, index) => (
+                    <li key={++index} className='property__inside-item'>
+                      {element}
+                    </li>
+                  ))}
                 </ul>
               </div>
+
               <div className='property__host'>
                 <h2 className='property__host-title'>Meet the host</h2>
                 <div className='property__host-user user'>
-                  <div className={`property__avatar-wrapper ${room.host.isPro ? 'property__avatar-wrapper--pro' : '' } user__avatar-wrapper`}>
+                  <div
+                    className={`property__avatar-wrapper ${
+                      room.host.isPro ? 'property__avatar-wrapper--pro' : ''
+                    } user__avatar-wrapper`}
+                  >
                     <img
                       className='property__avatar user__avatar'
                       src={room.host.avatarUrl}
@@ -129,163 +147,15 @@ export default function RoomPage(props: RoomPageProps): JSX.Element {
                     />
                   </div>
                   <span className='property__user-name'>{room.host.name}</span>
-                  {room.host.isPro ? <span className='property__user-status'>Pro</span> : null}
+                  {room.host.isPro ? (
+                    <span className='property__user-status'>Pro</span>
+                  ) : null}
                 </div>
                 <div className='property__description'>
-                  <p className='property__text'>
-                    {room.description}
-                  </p>
+                  <p className='property__text'>{room.description}</p>
                 </div>
               </div>
-              <section className='property__reviews reviews'>
-                <h2 className='reviews__title'>
-                  Reviews · <span className='reviews__amount'>1</span>
-                </h2>
-                <ul className='reviews__list'>
-                  <li className='reviews__item'>
-                    <div className='reviews__user user'>
-                      <div className='reviews__avatar-wrapper user__avatar-wrapper'>
-                        <img
-                          className='reviews__avatar user__avatar'
-                          src='img/avatar-max.jpg'
-                          width={54}
-                          height={54}
-                          alt='Reviews avatar'
-                        />
-                      </div>
-                      <span className='reviews__user-name'>Max</span>
-                    </div>
-                    <div className='reviews__info'>
-                      <div className='reviews__rating rating'>
-                        <div className='reviews__stars rating__stars'>
-                          <span style={{ width: '80%' }} />
-                          <span className='visually-hidden'>Rating</span>
-                        </div>
-                      </div>
-                      <p className='reviews__text'>
-                        A quiet cozy and picturesque that hides behind a a river
-                        by the unique lightness of Amsterdam. The building is
-                        green and from 18th century.
-                      </p>
-                      <time className='reviews__time' dateTime='2019-04-24'>
-                        April 2019
-                      </time>
-                    </div>
-                  </li>
-                </ul>
-                <form className='reviews__form form' action='#' method='post'>
-                  <label
-                    className='reviews__label form__label'
-                    htmlFor='review'
-                  >
-                    Your review
-                  </label>
-                  <div className='reviews__rating-form form__rating'>
-                    <input
-                      className='form__rating-input visually-hidden'
-                      name='rating'
-                      defaultValue={5}
-                      id='5-stars'
-                      type='radio'
-                    />
-                    <label
-                      htmlFor='5-stars'
-                      className='reviews__rating-label form__rating-label'
-                      title='perfect'
-                    >
-                      <svg className='form__star-image' width={37} height={33}>
-                        <use xlinkHref='#icon-star' />
-                      </svg>
-                    </label>
-                    <input
-                      className='form__rating-input visually-hidden'
-                      name='rating'
-                      defaultValue={4}
-                      id='4-stars'
-                      type='radio'
-                    />
-                    <label
-                      htmlFor='4-stars'
-                      className='reviews__rating-label form__rating-label'
-                      title='good'
-                    >
-                      <svg className='form__star-image' width={37} height={33}>
-                        <use xlinkHref='#icon-star' />
-                      </svg>
-                    </label>
-                    <input
-                      className='form__rating-input visually-hidden'
-                      name='rating'
-                      defaultValue={3}
-                      id='3-stars'
-                      type='radio'
-                    />
-                    <label
-                      htmlFor='3-stars'
-                      className='reviews__rating-label form__rating-label'
-                      title='not bad'
-                    >
-                      <svg className='form__star-image' width={37} height={33}>
-                        <use xlinkHref='#icon-star' />
-                      </svg>
-                    </label>
-                    <input
-                      className='form__rating-input visually-hidden'
-                      name='rating'
-                      defaultValue={2}
-                      id='2-stars'
-                      type='radio'
-                    />
-                    <label
-                      htmlFor='2-stars'
-                      className='reviews__rating-label form__rating-label'
-                      title='badly'
-                    >
-                      <svg className='form__star-image' width={37} height={33}>
-                        <use xlinkHref='#icon-star' />
-                      </svg>
-                    </label>
-                    <input
-                      className='form__rating-input visually-hidden'
-                      name='rating'
-                      defaultValue={1}
-                      id='1-star'
-                      type='radio'
-                    />
-                    <label
-                      htmlFor='1-star'
-                      className='reviews__rating-label form__rating-label'
-                      title='terribly'
-                    >
-                      <svg className='form__star-image' width={37} height={33}>
-                        <use xlinkHref='#icon-star' />
-                      </svg>
-                    </label>
-                  </div>
-                  <textarea
-                    className='reviews__textarea form__textarea'
-                    id='review'
-                    name='review'
-                    placeholder='Tell how was your stay, what you like and what can be improved'
-                    defaultValue={''}
-                  />
-                  <div className='reviews__button-wrapper'>
-                    <p className='reviews__help'>
-                      To submit review please make sure to set{' '}
-                      <span className='reviews__star'>rating</span> and describe
-                      your stay with at least{' '}
-                      <b className='reviews__text-amount'>50 characters</b>.
-                    </p>
-                    <button
-                      className='reviews__submit form__submit button'
-                      type='submit'
-                      disabled
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </form>
-              </section>
+              <CommentForm />
             </div>
           </div>
           <section className='property__map map' />
@@ -441,7 +311,6 @@ export default function RoomPage(props: RoomPageProps): JSX.Element {
             </div>
           </section>
         </div>
-
       </main>
     </div>
   ) : (
