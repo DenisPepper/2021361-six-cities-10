b4d1b8a2c4e508a81c60721aaf35d14be5a942d1
Перцев Денис <thech@yandex.ru>
2022-08-03 01:26:03 +0300

    modify const

--- project/src/const.ts
+++ project/src/const.ts
@@ -1,15 +1,15 @@
 export enum AuthorizationStatus {
-    Yes = 'YES',
-    No = 'NO',
-    Unknown = 'UNKNOWN'
+  Yes = 'YES',
+  No = 'NO',
+  Unknown = 'UNKNOWN',
 }
 
 export enum AppPath {
-    MainPage = '/',
-    LoginPage = '/login',
-    FavoritesPage = '/favorites',
-    Page404 = '*',
-    Offer = '/offer/'
+  MainPage = '/',
+  LoginPage = '/login',
+  FavoritesPage = '/favorites',
+  Page404 = '*',
+  Offer = '/offer/',
 }
 
 export const URL_MARKER_DEFAULT =
@@ -19,3 +19,20 @@ export const URL_MARKER_CURRENT =
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
 
 export const ICON_SIZE = 40;
+
+export const CITIES: string[] = [
+  'Paris',
+  'Cologne',
+  'Brussels',
+  'Amsterdam',
+  'Hamburg',
+  'Dusseldorf',
+];
+
+export const DEFAULT_CITY = 'Paris';
+
+export const DEFAULT_MAP_SETTINGS = {
+  latitude: 48.85661,
+  longitude: 2.351499,
+  zoom: 13,
+};
