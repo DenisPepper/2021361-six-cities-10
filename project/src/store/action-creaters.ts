90869c33c9c4794638a26868133d9ef6558a91a9
Перцев Денис <thech@yandex.ru>
2022-08-06 15:05:02 +0300

    modify action-creaters

--- project/src/store/action-creaters.ts
+++ project/src/store/action-creaters.ts
@@ -1,9 +1,10 @@
 import { createAction } from '@reduxjs/toolkit';
 import { OfferType } from '../types/offer-type';
-import {MapSettings} from '../types/map-types';
 
-export const changeCity = createAction('CHANGE_CITY', (payload: string) => ({payload}));
+export const changeCity = createAction('CHANGE_CITY', (payload: string) => ({
+  payload,
+}));
 
-export const setOffers = createAction('SET_OFFERS', (payload: OfferType[]) => ({payload}));
-
-export const setMapSettings = createAction('SET_MAP_SETTINGS', (payload: MapSettings) => ({payload}));
+export const setOffers = createAction('SET_OFFERS', (payload: OfferType[]) => ({
+  payload,
+}));
