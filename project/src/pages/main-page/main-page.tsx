4c83161241c9ba163b8161409ce3b6b073afe3f3
Перцев Денис <thech@yandex.ru>
2022-08-06 22:06:34 +0300

    add Sorts FC

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,6 +1,7 @@
 import Map from '../../components/map/map';
 import PlaceCardList from '../../components/place-card-list/place-card-list';
 import CitiesList from '../../components/cities-list/cities-list';
+import Sorts from '../../components/sorts/sorts';
 import { useAppSelector } from '../../hooks';
 import { filterOffersByCity } from '../../util';
 
@@ -68,32 +69,8 @@ export default function MainPage(props: MainPageProps): JSX.Element {
             <section className='cities__places places'>
               <h2 className='visually-hidden'>Places</h2>
               <b className='places__found'>{`${cityOffers.length} places to stay in City`}</b>
-              <form className='places__sorting' action='#' method='get'>
-                <span className='places__sorting-caption'>Sort by</span>
-                <span className='places__sorting-type' tabIndex={0}>
-                  Popular
-                  <svg className='places__sorting-arrow' width='7' height='4'>
-                    <use xlinkHref='#icon-arrow-select'></use>
-                  </svg>
-                </span>
-                <ul className='places__options places__options--custom places__options--opened'>
-                  <li
-                    className='places__option places__option--active'
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className='places__option' tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className='places__option' tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className='places__option' tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <Sorts />
+
               <div className='cities__places-list places__list tabs__content'>
                 <PlaceCardList cityOffers={cityOffers} isNearList={false} />
               </div>
