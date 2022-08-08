bedf7cdeff1d9ad8dcbc9d83851d61cbcb0f8fd7
Перцев Денис <thech@yandex.ru>
2022-08-08 21:34:47 +0300

    add middleware to store

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer';
+import { createHTTPClient } from '../services/api';
 
 const reducers = combineReducers({ reducer });
 
 export const store = configureStore({
   reducer: reducers,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({thunk:{extraArgument: createHTTPClient()}}),
 });
