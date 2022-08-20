5b811a231ff6b88860eee15aaafb2aa56d847174
Перцев Денис <thech@yandex.ru>
2022-08-20 15:19:53 +0300

    modify util

--- project/src/util.ts
+++ project/src/util.ts
@@ -1,2 +1,5 @@
-export const converToPercent = (rating: number) => rating * 100 / 5;
+import { DECIMAL, RATING_COEF } from './settings';
 
+export const converToPercent = (rating: number) => rating * RATING_COEF;
+
+export const getInteger = (value: string | undefined) => parseInt(String(value), DECIMAL);
