3e7d52f7a0039368b763a4a3b9c6e265093245f9
Перцев Денис <thech@yandex.ru>
2022-08-22 20:39:34 +0300

    modify LoginPage FC

--- project/src/pages/login-page/login-page.tsx
+++ project/src/pages/login-page/login-page.tsx
@@ -1,14 +1,18 @@
 import { Navigate } from 'react-router-dom';
-import Header from '../../components/header/header';
+
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { AuthorizationStatus, AppPath } from '../../settings';
 import { login } from '../../store/action-creaters-middleware';
+import LoginConteiner from '../../components/login-conteiner/login-conteiner';
 
-const throwError = (msg: string) => {throw new Error(msg);};
+const throwError = (msg: string) => {
+  throw new Error(msg);
+};
 
 type FormDataType = string | null | File;
 
-const assertString = (value: FormDataType, fieldName: string) => typeof value === 'string' ? value : throwError(`${fieldName} not valid`);
+const assertString = (value: FormDataType, fieldName: string) =>
+  typeof value === 'string' ? value : throwError(`${fieldName} not valid`);
 
 const validate = (form: FormData) => ({
   login: assertString(form.get('email'), 'email'),
@@ -29,56 +33,6 @@ export default function LoginPage(): JSX.Element {
   return isAuthorized ? (
     <Navigate to={AppPath.MainPage} />
   ) : (
-    <div className='page page--gray page--login'>
-      <Header isLoginPage />
-      <main className='page__main page__main--login'>
-        <div className='page__login-container container'>
-          <section className='login'>
-            <h1 className='login__title'>Sign in</h1>
-            <form
-              onSubmit={onSubmitHandler}
-              className='login__form form'
-              action='#'
-              method='post'
-            >
-              <div className='login__input-wrapper form__input-wrapper'>
-                <label className='visually-hidden'>E-mail</label>
-                <input
-                  className='login__input form__input'
-                  type='email'
-                  name='email'
-                  placeholder='Email'
-                  required
-                />
-              </div>
-              <div className='login__input-wrapper form__input-wrapper'>
-                <label className='visually-hidden'>Password</label>
-                <input
-                  className='login__input form__input'
-                  type='password'
-                  name='password'
-                  placeholder='Password'
-                  required
-                />
-              </div>
-              <button
-                className='login__submit form__submit button'
-                type='submit'
-              >
-                Sign in
-              </button>
-            </form>
-          </section>
-          <section className='locations locations--login locations--current'>
-            <div className='locations__item'>
-              {/*FIXME: почему Амстердам?*/}
-              <a className='locations__item-link' href='#ref'>
-                <span>Amsterdam</span>
-              </a>
-            </div>
-          </section>
-        </div>
-      </main>
-    </div>
+    <LoginConteiner callback={onSubmitHandler} />
   );
 }
