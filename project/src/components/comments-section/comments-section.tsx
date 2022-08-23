3bd38bc98783bca24e2d7fd872d6446a87b99ff0
Перцев Денис <thech@yandex.ru>
2022-08-24 00:56:00 +0300

    add sorting by date in CommentSection FC

--- project/src/components/comments-section/comments-section.tsx
+++ project/src/components/comments-section/comments-section.tsx
@@ -3,12 +3,15 @@ import { AuthorizationStatus } from '../../settings';
 import { CommentType } from '../../types/comment-type';
 import CommentForm from '../comment-form/comment-form';
 import CommentItem from '../comment-item/comment-item';
+import dayjs from 'dayjs';
 
 type CommentFormProps = {
   id: number
 };
 
 const generateKey = (comment: CommentType): string => `${comment.id}${comment.user.id}${comment.date}`;
+
+const comparator = (a: CommentType, b: CommentType) => dayjs(b.date).diff(dayjs(a.date));
 
 export default function CommentSection(props: CommentFormProps): JSX.Element {
   const { id } = props;
@@ -22,7 +25,7 @@ export default function CommentSection(props: CommentFormProps): JSX.Element {
       </h2>
 
       <ul className='reviews__list'>
-        {comments.slice(0, 10).map((comment) => (
+        {comments.slice(0, 10).sort(comparator).map((comment) => (
           <CommentItem key={generateKey(comment)} comment={comment} />
         ))}
       </ul>
