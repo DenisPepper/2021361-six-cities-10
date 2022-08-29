71bf798de6cf2937aadbfa4a7b80a4dae2fec509
Перцев Денис <thech@yandex.ru>
2022-08-29 13:44:47 +0300

    add current-slice reducers tests

--- project/src/store/slices/currents-slice/currents-slice.test.ts
+++ project/src/store/slices/currents-slice/currents-slice.test.ts
@@ -1,16 +1,37 @@
-import {
-  currentSlice,
-} from './currents-slice';
+import { currentSlice, setCurrentCity, setCurrentID, setCurrentSort } from './currents-slice';
+
+const reducer = currentSlice.reducer;
 
 const unknowCase = { type: 'UNKNOW_CASE' };
+
 const defaultStore = {
   currentcity: 'Paris',
   currentID: NaN,
   currentSort: 'Popular',
 };
 
-describe('reducer at currentSlice', () => {
-  it('when default case', () => {
-    expect(currentSlice.reducer(undefined, unknowCase)).toEqual(defaultStore);
-  });
+describe('current-slice reducer', () => {
+  it('when default case', () =>
+    expect(reducer(undefined, unknowCase)).toEqual(defaultStore));
+
+  it('when set current city as string then success', () =>
+    expect(reducer(defaultStore, setCurrentCity('someCity'))).toEqual({
+      currentcity: 'someCity',
+      currentID: NaN,
+      currentSort: 'Popular',
+    }));
+
+  it('when set current ID as number then success', () => expect(reducer(defaultStore, setCurrentID(5))).toEqual({
+    currentcity: 'Paris',
+    currentID: 5,
+    currentSort: 'Popular',
+  }));
+
+  it('when set current sort as string then success', () => expect(reducer(defaultStore, setCurrentSort('someSort'))).toEqual({
+    currentcity: 'Paris',
+    currentID: NaN,
+    currentSort: 'someSort',
+  }));
+
 });
+
